17f3a6a4dd7556c1ebb42a773db1c9f0c186d18c
annabellaMalatinszki <a.malatinszki@gmail.com>
2017-11-01 16:03:50 +0100

    Implement info-retrieving functions

---
+++ ProcessNote/ProcessNote/CurrentProcess.cs
@@ -0,0 +1,97 @@
+﻿using System.Diagnostics;
+using System;
+
+namespace ProcessNote
+{
+    internal class CurrentProcess
+
+    {
+        private int id;
+        private string name;
+        private float cpuUsage;
+        private float memoryUsage;
+        private TimeSpan runningTime;
+        private DateTime startTime;
+        private ProcessThreadCollection threads;
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public float CpuUsage
+        {
+            get { return cpuUsage; }
+        }
+
+        public float MemoryUsage
+        {
+            get { return memoryUsage; }
+        }
+
+        public TimeSpan RunningTime
+        {
+            get { return runningTime; }
+        }
+
+        public DateTime StartTime
+        {
+            get { return startTime; }
+        }
+
+        public ProcessThreadCollection Threads
+        {
+            get { return threads; }
+        }
+
+
+        public CurrentProcess(int processId)
+        {
+            id = processId;
+            name = Process.GetProcessById(processId).ProcessName;
+            cpuUsage = CountCPUUsage();
+            memoryUsage = CountMemoryUsage();
+            startTime = RetrieveStartTime();
+            runningTime = CountRunningTime();
+            threads = RetrieveThreads();
+
+        }
+
+        private float CountCPUUsage()
+        {
+            PerformanceCounter theCPUCounter = new PerformanceCounter("Process", "% Processor Time", name);
+            float CPUCounterValue = theCPUCounter.NextValue();
+            System.Threading.Thread.Sleep(1000);
+            return theCPUCounter.NextValue();
+        }
+
+        private float CountMemoryUsage()
+        {
+            PerformanceCounter theMemCounter = new PerformanceCounter("Memory", "Available MBytes");
+            //Console.WriteLine("MEMORY: " + theMemCounter.NextValue());
+            return theMemCounter.NextValue();
+
+        }
+
+        private DateTime RetrieveStartTime()
+        {
+            return Process.GetProcessById(id).StartTime;
+        }
+
+        private TimeSpan CountRunningTime()
+        {
+            DateTime now = DateTime.Now;
+            return (now-startTime);
+        }
+
+        private ProcessThreadCollection RetrieveThreads() 
+        {
+            return Process.GetProcessById(id).Threads;
+        }
+    }
+}
--- ProcessNote/ProcessNote/Program.cs
+++ ProcessNote/ProcessNote/Program.cs
@@ -16,6 +16,31 @@ namespace ProcessNote
 
             Console.WriteLine("CURRENTLY RUNNING PROCESSES:\n" + allProcesses.ToString());
 
+
+                        Console.WriteLine("SELECT AN ID TO SEE DETAILS: ");
+
+            int selectedProcessId;
+            if (int.TryParse(Console.ReadLine(), out selectedProcessId))
+            {
+                currentProcess = new CurrentProcess(selectedProcessId);
+                StringBuilder sb = new StringBuilder();
+                sb.Append("\nName: " + currentProcess.Name);
+                sb.Append("\nCPU usage: " + currentProcess.CpuUsage);
+                sb.Append("\nMemory usage: " + currentProcess.MemoryUsage);
+                sb.Append("\nStart time: " + currentProcess.StartTime);
+                sb.Append("\nRunning time: " + currentProcess.RunningTime);
+                sb.Append("\nThreads: ");
+                foreach(ProcessThread thread in currentProcess.Threads) 
+                {
+                    sb.Append("\nId: " + thread.Id + " Current priority: " + thread.CurrentPriority);
+                }
+
+                Console.WriteLine(sb);
+            }
+            else
+            {
+                Console.WriteLine("PROCESS ID NOT FOUND");
+            }
             Console.ReadKey();
         }
     }
