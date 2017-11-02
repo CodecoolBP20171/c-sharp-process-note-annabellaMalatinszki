9638b3ed3c88d9fb99e5a543776e1aad39674ce7
annabellaMalatinszki <a.malatinszki@gmail.com>
2017-11-02 11:29:20 +0100

    Create basic window structure

---
+++ ProcessNoteWinForms/ProcessNoteWinForms/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ProcessNoteWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.processList = new System.Windows.Forms.ListView();
+            this.processListLabel = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.processName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // processList
+            // 
+            this.processList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.processId,
+            this.processName});
+            this.processList.Location = new System.Drawing.Point(28, 52);
+            this.processList.Name = "processList";
+            this.processList.Size = new System.Drawing.Size(363, 234);
+            this.processList.TabIndex = 0;
+            this.processList.UseCompatibleStateImageBehavior = false;
+            this.processList.SelectedIndexChanged += new System.EventHandler(this.processList_SelectedIndexChanged);
+            // 
+            // processListLabel
+            // 
+            this.processListLabel.AutoSize = true;
+            this.processListLabel.Location = new System.Drawing.Point(25, 36);
+            this.processListLabel.Name = "processListLabel";
+            this.processListLabel.Size = new System.Drawing.Size(143, 13);
+            this.processListLabel.TabIndex = 1;
+            this.processListLabel.Text = "Currenctly running processes";
+            this.processListLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(290, 305);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(101, 23);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "Close Process";
+            this.closeButton.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(426, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newProcessToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // newProcessToolStripMenuItem
+            // 
+            this.newProcessToolStripMenuItem.Name = "newProcessToolStripMenuItem";
+            this.newProcessToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newProcessToolStripMenuItem.Text = "New Process";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // processId
+            // 
+            this.processId.Text = "Process Id";
+            // 
+            // processName
+            // 
+            this.processName.Text = "Process Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 342);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.processListLabel);
+            this.Controls.Add(this.processList);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView processList;
+        private System.Windows.Forms.Label processListLabel;
+        private System.Windows.Forms.ColumnHeader processId;
+        private System.Windows.Forms.ColumnHeader processName;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newProcessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ ProcessNoteWinForms/ProcessNoteWinForms/ProcessHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace ProcessNoteWinForms
+{
+    internal class ProcessHandler
+    {
+        public Process[] RunningProcesses { get; set; }
+
+        public ProcessHandler()
+        {
+            RunningProcesses = Process.GetProcesses();
+        }
+    }
+}
